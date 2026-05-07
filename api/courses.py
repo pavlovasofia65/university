39c786185c86e01a708931358727e19dc2a1a4b1
pavlovasofia65 <pavlovasofia65@gmail.com>
2026-05-07 11:49:24 +0300

    began working on routers

--- api/courses.py
+++ api/courses.py
@@ -1,8 +1,23 @@
+# Not sure to do this at all
 from flask import Blueprint, render_template
 from models import Course
 
 courses_bp = Blueprint('courses', __name__)
+Course = Course()
 
 @courses_bp.route('/courses')
 def list_courses():
+    courses = Course.query.order_by()
     return render_template('courses/courses.html')
+
+@courses_bp.route('/courses/<int:id>')
+def course_details():
+    return render_template('courses/course.html')
+
+@courses_bp.route('/courses/create-course', methods=['POST', 'GET'])
+def create_course():
+    return render_template('/courses/create_course.html')
+
+@courses_bp.route('/courses/delete')
+def delete_course():
+    return
--- api/disciplines.py
+++ api/disciplines.py
@@ -6,3 +6,7 @@ disciplines_bp = Blueprint('disciplines', __name__)
 @disciplines_bp.route('/disciplines')
 def list_disciplines():
     return render_template('disciplines/disciplines.html')
+
+@disciplines_bp.route('/disciplines/<int:id>')
+def discipline_details():
+    return render_template('disciplines/discipline')
